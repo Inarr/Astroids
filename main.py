5e80c1864c8e8bcc31ec16ca17d3a3223446c69b
Inarr Dobrey <inarrd@gmail.com>
2024-10-02 19:45:49 -0400

    Correct Version

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+
 def main():
-    print("Starting Astroids")
+    print("Starting asteroids!")
+
+if __name__ == "__main__":
+    main()
