6501b6def572967e388e701ffa2092b4302dcda4
Inarr Dobrey <inarrd@gmail.com>
2024-10-07 19:01:42 -0400

    B: Imported from constants

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
     main()
